a3d1c918e3ef54dd449066dac7ae71db573c80d4
Maciej Cąderek <maciej.caderek@gmail.com>
2020-04-16 15:59:09 +0200

    Fixed panning sensibility

--- src/index.ts
+++ src/index.ts
@@ -311,6 +311,7 @@ const main = async () => {
   scene.activeCamera.inertia = 0
   scene.activeCamera.checkCollisions = true
   scene.activeCamera.panningInertia = 0
+  scene.activeCamera.panningSensibility = 50
 
   const camera = scene.cameras[scene.cameras.length - 1]
   // const ambientOcclusion = new AmbientOcclusion(scene, camera)
